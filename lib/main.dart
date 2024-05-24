0b113be088d58707dca372970df3507d1383d62b
sobhan <sobhan.ziaee73@gmail.com>
2024-05-24 18:12:01 +0330

    remove un used import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:bmi_calculator/constant/colors.dart';
 import 'package:bmi_calculator/screens/home_screen.dart';
-import 'package:bmi_calculator/widgets/left_bg_shape.dart';
-import 'package:bmi_calculator/widgets/right_bg_shape.dart';
 import 'package:flutter/material.dart';
 
 void main() {
